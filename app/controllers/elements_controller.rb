f4247bdabd4d4f201a1179f51e215adb23b93784
Ksusha <spas.ksusha@gmail.com>
2022-12-11 16:18:49 +0300

    add form

--- app/controllers/elements_controller.rb
+++ app/controllers/elements_controller.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal: true
 
+require 'nokogiri'
+require 'open-uri'
+
 # class of controller
 class ElementsController < ApplicationController
+  include ElementsHelper
+  before_action :check_data, only: :result
+  before_action :set_values, only: :result
+  URL_SERV = 'http://localhost:3000/?format=xml'
   def index; end
 
   def result
+    url_with_params = URL_SERV + "&length=#{@length}&str_arr=#{@str.split.join('+')}"
+    server_response = URI.open(url_with_params)
+    render xml: server_response
   end
 end
--- app/helpers/elements_helper.rb
+++ app/helpers/elements_helper.rb
@@ -2,4 +2,18 @@
 
 # helper
 module ElementsHelper
+  def check_data
+    if /[^\d^\s]/.match(params[:str_arr]) then redirect_to home_path, notice: "Sequence isn't correct"
+    else
+      arr = params[:str_arr].split.map!(&:to_i)
+      redirect_to home_path, notice: "Length isn't correct" if arr.length != params[:length].to_i
+    end
+     flash[:notice] = 'The mode must be selected' if params[:mode].nil?
+  end
+
+  def set_values
+    @str = params[:str_arr]
+    @length = params[:length].to_i
+    @mode = params[:mode]
+  end
 end
